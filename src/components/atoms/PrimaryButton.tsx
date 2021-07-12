4629923bcd441f8153b3cc8f92989e001de6f1fc
raorao1011 <raorao1011@gmail.com>
2021-07-12 12:20:54 +0900

    ✨ PrimaryButtonの作成

---
+++ src/components/atoms/PrimaryButton.tsx
@@ -0,0 +1,9 @@
+import { ReactNode, FC } from "react";
+
+type PrimaryButton = {
+  children: ReactNode;
+};
+
+export const PrimaryButton: FC<PrimaryButton> = ({ children }): JSX.Element => {
+  return <button className="w-1/6 flex items-center justify-center rounded-md bg-black text-white p-3">{children}</button>;
+};
