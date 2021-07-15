e1fa5fedc6068eb470e01b5fd4f20d665ce028e5
raorao1011 <raorao1011@gmail.com>
2021-07-15 16:36:01 +0900

    ✨ useWeatherフック作成

--- src/hooks/useWeatherApi.ts
+++ src/hooks/useWeatherApi.ts
@@ -1,17 +1,20 @@
 import axios from "axios";
 import { useCallback, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCoordinate, setCoordinate } from "src/store/geolocationSlice";
 import { changeFlag } from "../store/weatherSlice";
 
-export const useWeatherApi = (): any => {
+// WeatherApiを用いて天気情報を取得します。
+export const useWeatherApi = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [weatherData, setWeatherData] = useState([]);
   const dispatch = useDispatch();
+  const coordinate = useSelector(selectCoordinate);
 
-  // WeatherApiを用いて天気情報を取得します。
-  const getWeatherData = useCallback(async () => {
-    console.log("getWeatherData start");
-    
+  // 位置情報が使用できない際に東京の天気を表示する
+  const getTokyoWeatherData = useCallback(async () => {
+    console.log("getTokyoWeatherData start");
+
     // 末尾に&units=metricをつけると摂氏表記になる
     const URL = `${process.env.NEXT_PUBLIC_OW_API_URL}/weather/?q=Tokyo&APPID=${process.env.NEXT_PUBLIC_OW_API_KEY}&units=metric`;
     dispatch(changeFlag(true));
@@ -28,5 +31,18 @@ export const useWeatherApi = (): any => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return { getWeatherData, isLoading, weatherData };
+  const fetchWeatherData = useCallback(async () => {
+    console.log("fetchWeatherDataStart");
+
+    const URL = `${process.env.NEXT_PUBLIC_OW_API_URL}/weather/?lat=${coordinate.lat}&lon=${coordinate.lng}appid=${process.env.NEXT_PUBLIC_OW_API_KEY}&units=metric`;
+
+    try {
+      const res = await axios.get(URL);
+      setWeatherData(res.data)
+    } catch (error) {
+      console.warn(error.message)
+    }
+  }, []);;
+
+  return { getTokyoWeatherData, fetchWeatherData, isLoading, weatherData };
 };
