ec5f0744eb4021bfe6575d3da7195dc56a4ec6f2
raorao1011 <raorao1011@gmail.com>
2021-08-02 22:12:16 +0900

    ✨ urlを追加

--- src/lib/urls.ts
+++ src/lib/urls.ts
@@ -1,5 +1,11 @@
 // 東京の天気を取得URL
-export const TOKYO_WEATHER_URL = `${process.env.NEXT_PUBLIC_OW_API_URL}/weather/?q=Tokyo&APPID=${process.env.NEXT_PUBLIC_OW_API_KEY}&units=metric`
+export const TOKYO_WEATHER_URL = `${process.env.NEXT_PUBLIC_OW_API_URL}/weather/?q=Tokyo&APPID=${process.env.NEXT_PUBLIC_OW_API_KEY}&units=metric`;
 
 // Geocoding URL
-export const GEOCODING_URL = "https://maps.google.com/maps/api/geocode/json?address="
+export const GEOCODING_URL = "https://maps.google.com/maps/api/geocode/json?address=";
+
+// ONECALL API
+export const ONEALLAPI_URL = "https://api.openweathermap.org/data/2.5/onecall";
+
+// OPEN WEATHER ICON URL 語尾に`${iconID}@2x.png`を付与
+export const OPEN_WEATHER_ICON_URL = "http://openweathermap.org/img/wn";
